c78d4e7a6879f9881df18c77cf0a909f58d8644c
aviya oren <98823130+OrenAviya@users.noreply.github.com>
2023-05-24 10:34:09 +0300

    work , but not exit the program Completely

--- EX4/better_ping.py
+++ EX4/better_ping.py
@@ -33,75 +33,90 @@ def better_ping(host):
     # Create a raw socket 
     sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
     if isinstance(sock, socket.socket):
-        print("socket created successfully\n")
+        print("socket ping created successfully\n")
     else:
         print("socket creation failed. \n")
     
     #send a ping + get a response:
 
-    while True:
-        # Create the ICMP echo request packet
-        pid = os.getpid() & 0xFFFF
-        ch_sum = 0 
-        head = struct.pack("!BBHHH", ICMP_ECHO_REQUEST, 0, ch_sum, pid, count_iter)
-        # start_time = time.time()
-        data = struct.pack("d",time.time())
-        # Calculate the checksum for the packet
-        my_checksum = checksum(head + data)
-        # create the icmp packet for sending
-        head = struct.pack("!BBHHH", ICMP_ECHO_REQUEST, 0, socket.htons(my_checksum), pid, count_iter)
-        icmp_packet = head + data
-
-        #  Send the packet
-        destIP = (host, count_iter)
-        try:
-            sock.sendto(icmp_packet, destIP )    
-        except socket.error as e:
-            print(f"Socket error send failed: {e}")
-        
-        while True:
-            print("in while ping")
-            # Receive the response packet
-            try:
-                data, addr = sock.recvfrom(1024)
-            except socket.error as e:
-                print(f"Socket error recived failed: {e}")
-            
-
-            # Extract the ICMP type and code from the response packet
-            icmp_type, icmp_code = struct.unpack('!BB', data[20:22])
-            
-            if icmp_type == ICMP_ECHO_REPLY and icmp_code == 0:
-                
-                # Extract (for printing) the number of bytes (len), IP, sequence number, and time from the response packet
-                ip = addr[0]
-                seq_num_reply = struct.unpack('!H', data[6:8])[0]
-                len_data = len(data)
-                time_data = struct.unpack('!f', data[28:32])  # Assuming the time field is a float (4 bytes)
-                elapsed_time= time_data[0] 
-                print(f"{len_data} bytes from {ip}: icmp_seq={seq_num_reply} time={elapsed_time}s")
-                break
+    # while True: no need , want to do it one time
+    # Create the ICMP echo request packet
+    pid = os.getpid() & 0xFFFF
+    ch_sum = 0 
+    head = struct.pack("!BBHHH", ICMP_ECHO_REQUEST, 0, ch_sum, pid, count_iter)
+    # start_time = time.time()
+    data = struct.pack("d",time.time())
+    # Calculate the checksum for the packet
+    my_checksum = checksum(head + data)
+    # create the icmp packet for sending
+    head = struct.pack("!BBHHH", ICMP_ECHO_REQUEST, 0, socket.htons(my_checksum), pid, count_iter)
+    icmp_packet = head + data
+    print("created the icmp_mes")
+    #  Send the packet
+    destIP = (host, count_iter)
+    try:
+        sock.sendto(icmp_packet, destIP )
+        print(f"Socket send file")    
+    except socket.error as e:
+        print(f"Socket error send failed: {e}")
     
+    while True:
+        # Receive the response packet
+        try:
+            print(f"Socket wait for data")
+            data, addr = sock.recvfrom(1024)
+            print(f"data: {data} ,addres {addr}")
+        except socket.error as e:
+            print(f"Socket error recived failed: {e}")
+            return 0
+        
+
+        # Extract the ICMP type and code from the response packet
+        icmp_type, icmp_code = struct.unpack('!BB', data[20:22])
+        print(f"icmptype = {icmp_type} , icmpcode = {icmp_code}")
+        if icmp_type == ICMP_ECHO_REPLY and icmp_code == 0:
+            
+            # Extract (for printing) the number of bytes (len), IP, sequence number, and time from the response packet
+            ip = addr[0]
+            seq_num_reply = struct.unpack('!H', data[6:8])[0]
+            len_data = len(data)
+            time_data = struct.unpack('!f', data[28:32])  # Assuming the time field is a float (4 bytes)
+            elapsed_time= time_data[0] 
+            print(f"{len_data} bytes from {ip}: icmp_seq={seq_num_reply} time={elapsed_time}s")
+            return 1
+            break
+        else :
+            print("error")
+
 if __name__ == '__main__':
-    host = '8.8.8.8'
+    
+    host = '1.2.3.4'
     # start the watchdog
     t_watchdog = threading.Thread(target=lambda: os.system(f'python3 watch_dog.py'))
     t_watchdog.start()
     
-    better_ping(host)
-
-    # Connect to watchdog
-    sock2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    if isinstance(sock2, socket.socket):
-        print("socket created successfully\n")
-    else:
-        print("socket creation failed. \n")
+    time.sleep(1)
     try:
-        sock2.connect(('localhost', 2000))
+        while True:
+            is_sent = better_ping(host)
+            print(f"issent={is_sent}")
+            # Connect to watchdog
+            sock2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            if isinstance(sock2, socket.socket):
+                print("socket created successfully\n")
+            else:
+                print("socket creation failed. \n")
+
+            if(sock2.connect_ex(('localhost', 2000))==0):
+                print("socket connected successfully\n")
+            else:
+                print(f"Socket connect failed")
+            if (is_sent == 1):
+                # send ok to watch dog if we get response
+                message = "got responed"
+                sock2.send(message.encode())
+
     except socket.error as e:
-        print(f"Socket connect failed: {e}")
-    # send ok to watch dog if we get response
-    message = "got responed"
-    sock2.send(message.encode())
-    sock2.close()
+        sock2.close()
+
 
